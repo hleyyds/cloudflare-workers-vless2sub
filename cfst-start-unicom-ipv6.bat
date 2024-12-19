fc9a3ee2298593c266a8aef2445195bb196e056e
hleyyds <118107495+hleyyds@users.noreply.github.com>
2024-12-19 19:59:08 +0800

    init

---
+++ cfst-start-unicom-ipv6.bat
@@ -0,0 +1,2 @@
+@echo off
+cfst.exe -url https://speedtest.hix.us.kg -f ipv6-s.txt -o "unicom-ipv6.csv"
---
+++ cfst-start-unicom.bat
@@ -0,0 +1,2 @@
+@echo off
+cfst.exe -url https://speedtest.hix.us.kg -f ip.txt -o "unicom.csv"
